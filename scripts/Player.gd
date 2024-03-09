3d4d4a8f2e35db0a2dc730fca93bc417fb684cb2
craftablescience <me@craftablescience.info>
2024-03-08 22:11:40 -0500

    feat: add quake 3-style player movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,23 +1,33 @@
 extends CharacterBody3D
 
+# Player movement code is modified from https://aneacsu.com/blog/2023/04/09/quake-movement-godot
 
-@export var SPEED_WALK := 10.0
-@export var SPEED_SPRINT := 32.0 # Debugging moment
-@export var SENSITIVITY := 0.1
-@export var HOLD_LERP_SPEED := 10
+const MAX_VEL_AIR := 0.6
+const MAX_VEL_GROUND := 6.0
+const MAX_ACCEL := 10 * MAX_VEL_GROUND
+const FRICTION := 4
+const STOP_SPEED := 1.5
+const WALK_MULTIPLIER := 0.8
 
-@export var FOOTSTEP_MIN_WAIT_TIME := 0.1
-@export var FOOTSTEP_MAX_WAIT_TIME := 0.2
-@export var AUDIO_RAND_MIN_PITCH_SCALE := 0.9
-@export var AUDIO_RAND_MAX_PITCH_SCALE := 1.1
+const MOUSE_SENSITIVITY := 0.1
 
-@onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+const HOLD_LERP_SPEED := 10
 
-@onready var footstep_active := false
-@onready var RNG := RandomNumberGenerator.new()
+const FOOTSTEP_MIN_WAIT_TIME := 0.1
+const FOOTSTEP_MAX_WAIT_TIME := 0.2
+const AUDIO_RAND_MIN_PITCH_SCALE := 0.9
+const AUDIO_RAND_MAX_PITCH_SCALE := 1.1
 
-@onready var held: RigidBody3D = null
-@onready var held_parent: Node = null
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var jump_force = sqrt(gravity * 6)
+var is_jump_requested := false
+var is_walking := false
+
+var footstep_active := false
+var RNG := RandomNumberGenerator.new()
+
+var held: RigidBody3D = null
+var held_parent: Node = null
 
 
 func held_is_valid() -> bool:
@@ -27,10 +37,6 @@ func held_is_valid() -> bool:
 func reset() -> void:
 	%Camera.current = true
 	%RotationHelper.rotation_degrees.x = 0
-
-
-func get_input_direction() -> Vector2:
-	return Input.get_vector("mv_left", "mv_right", "mv_fwd", "mv_back")
 
 
 func set_ui_visible(vis: bool) -> void:
@@ -47,30 +53,61 @@ func show_ui() -> void:
 	set_ui_visible(true)
 
 
+func collect_inputs() -> Vector3:
+	is_jump_requested = Input.is_action_just_pressed("mv_jump")
+	is_walking = Input.is_action_pressed("mv_walk")
+	
+	var input_dir := Input.get_vector("mv_left", "mv_right", "mv_fwd", "mv_back")
+	return (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+
+func update_velocity(delta: float) -> void:
+	var wish_dir := collect_inputs()
+	if is_on_floor():
+		if is_jump_requested:
+			velocity.y = jump_force
+			velocity = update_velocity_air(wish_dir, delta)
+		else:
+			if is_walking:
+				velocity.x *= WALK_MULTIPLIER
+				velocity.z *= WALK_MULTIPLIER
+			velocity = update_velocity_ground(wish_dir, delta)
+	else:
+		velocity.y -= gravity * delta
+		velocity = update_velocity_air(wish_dir, delta)
+
+
+func accelerate(wish_dir: Vector3, max_speed: float, delta: float) -> Vector3:
+	var current_speed = velocity.dot(wish_dir)
+	return velocity + clamp(max_speed - current_speed, 0, MAX_ACCEL * delta) * wish_dir
+
+
+func update_velocity_ground(wish_dir: Vector3, delta: float) -> Vector3:
+	var speed = velocity.length()
+	if speed != 0:
+		var control = max(STOP_SPEED, speed)
+		var drop = control * FRICTION * delta
+		velocity *= max(speed - drop, 0) / speed
+	return accelerate(wish_dir, MAX_VEL_GROUND, delta)
+
+
+func update_velocity_air(wish_dir: Vector3, delta: float) -> Vector3:
+	return accelerate(wish_dir, MAX_VEL_AIR, delta)
+
+
 func _process(_delta: float) -> void:
-	var input_dir := get_input_direction()
-	if not footstep_active and (input_dir.x != 0.0 or input_dir.y != 0.0) and %Camera.current:
+	if not footstep_active and is_on_floor() and get_real_velocity().length() > 0.0 and %Camera.current:
 		footstep_active = true
 		$Footstep/FootstepTimer.start(RNG.randf_range(FOOTSTEP_MIN_WAIT_TIME, FOOTSTEP_MAX_WAIT_TIME))
 
 
 func _physics_process(delta: float) -> void:
-	var speed := SPEED_SPRINT if Globals.DEBUG_OPTS["can_sprint"] and Input.is_action_pressed("mv_sprint") else SPEED_WALK	
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
-	# Get the input direction and handle the movement/deceleration.
-	var input_dir := get_input_direction()
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
-	else:
-		var smoothing := pow(speed, 2) * delta;
-		velocity.x = move_toward(velocity.x, 0, smoothing)
-		velocity.z = move_toward(velocity.z, 0, smoothing)
+	# Handle movement
+	update_velocity(delta)
 	
 	# Move held object
 	if held_is_valid():
@@ -84,11 +121,11 @@ func _physics_process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		var x_rotation: float = event.relative.y * SENSITIVITY
+		var x_rotation: float = event.relative.y * MOUSE_SENSITIVITY
 		if not Globals.invert_y:
 			x_rotation *= -1
 		%RotationHelper.rotate_x(deg_to_rad(x_rotation))
-		rotate_y(deg_to_rad(-event.relative.x * SENSITIVITY))
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY))
 		var camera_rot: Vector3 = %RotationHelper.rotation_degrees
 		camera_rot.x = clamp(camera_rot.x, -90, 90)
 		%RotationHelper.rotation_degrees = camera_rot
